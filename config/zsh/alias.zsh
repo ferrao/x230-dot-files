fea7a945be2902a6c2b51a473145360169355cfb
Rui Ferrão <mail@ruiferrao.com>
2019-03-06 23:47:12 +0000

    fixed console issues

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -3,7 +3,9 @@
 #
 
 alias reboot="sudo reboot"
-alias halt="sudo shutdown -h now"
+alias halt="sudo poweroff"
 alias more="less"
 alias vim="nvim" 
 alias vimconfig="nvim ~/.config/nvim/init.vim"
+alias clear="clear"
+alias claer="clear"
---
+++ config/zsh/font.zsh
@@ -0,0 +1,2 @@
+# Unable to set powerline font console during boot
+setfont ter-powerline-v16n
