6ac0043c89de7eee2dfc5f8937f9fc29210e46d6
Rui Ferrão <mail@ruiferrao.com>
2020-04-08 10:22:11 +0100

    monitor bar app menu, nocaps alias, variables and path updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,13 +9,14 @@ alias vimconfig="nvim ~/.config/nvim/init.vim"
 alias fishconfig="nvim ~/.config/fish/config.fish"
 alias clear="clear"
 alias claer="clear"
+alias nocaps="setxkbmap -layout us -option ctrl:nocaps"
 
 # Starship
 starship init fish | source
 
 # Sane auto completion key bindings
-bind \c] accept-autosuggestion 
-bind \t complete 
+bind \c] accept-autosuggestion
+bind \t complete
 
 # make sure awt works with bspwm
 set -U _JAVA_AWT_WM_NONREPARENTING 1
@@ -24,7 +25,10 @@ set -U _JAVA_AWT_WM_NONREPARENTING 1
 set -U PASSWORD_STORE_X_SELECTION primary
 
 # Local bin Path, includes python, aws, others.
-set PATH /home/ferrao/.local/bin $PATH
+set PATH /home/ferrao/.local/bin ./bin $PATH
 
 # Ruby Path
 set PATH /home/ferrao/.gem/ruby/2.6.0/bin $PATH
+
+# Idea Path
+set PATH /opt/idea-IU-193.6494.35/bin $PATH
